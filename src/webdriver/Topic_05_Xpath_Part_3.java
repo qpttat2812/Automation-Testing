73d4a413d58cbeffeac6fe9b97f920e0f6ed5c48
ttqp2812fi <qptt2812@gmail.com>
2023-07-21 10:58:05 +0700

    updating tc_02

--- src/webdriver/Topic_05_Xpath_Part_3.java
+++ src/webdriver/Topic_05_Xpath_Part_3.java
@@ -1,5 +1,67 @@
 package webdriver;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Topic_05_Xpath_Part_3 {
-a
+	
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	
+	@BeforeClass
+	public void BeforeClass() {
+		if(osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+		else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+		
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	
+	}
+	
+	
+	@Test
+	public void TC_01_Empty_Data() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		//get css selector for button 1
+//		driver.findElement(By.cssSelector("button[class='btn_pink_sm fs16']")).click();
+		
+		//get css selector for button 2
+		driver.findElement(By.cssSelector("button.btn_pink_sm.fs16")).click();
+		
+		//get xpath for button
+//		driver.findElement(By.xpath("//button[@class='btn_pink_sm fs16']")).click();
+		
+		//check error messages
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtFirstname-error']")).getText(), "Vui lòng nhập họ tên");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtEmail-error']")).getText(), "Vui lòng nhập email");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtCEmail-error']")).getText(), "Vui lòng nhập lại địa chỉ email");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtPassword-error']")).getText(), "Vui lòng nhập mật khẩu");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtCPassword-error']")).getText(), "Vui lòng nhập lại mật khẩu");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[@id='txtPhone-error']")).getText(), "Vui lòng nhập số điện thoại.");
+		
+	}
+	
+	@Test
+	public void TC_02_Invalid_Email() {
+		driver.findElement(By.id("txtFirstname")).sendKeys("Quynh");
+		driver.findElement(By.name("txtEmail")).sendKeys("Pham");
+	}
+	
+	@AfterClass
+	public void AfterClass() {
+		driver.quit();
+	}
 }
