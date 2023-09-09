5fa7102c1d6be0664bbbe50142333e5f7f848fc8
ttqp2812fi <qptt2812@gmail.com>
2023-09-09 22:17:24 +0700

    initialize topic 15-popup

---
+++ src/webdriver/Topic_15_Popup.java
@@ -0,0 +1,39 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_15_Popup {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	
+	@BeforeClass
+	public void BeforeClass() {
+		if(osName.contains("Window")) {
+			System.setProperty("webdriver.gecko.driver", projectPath +  "\\browserDrivers\\geckodriver.exe");
+		}
+		else {
+			System.setProperty("webdriver.gecko.driver", projectPath +  "/browserDrivers/geckodriver.exe");
+		}
+		
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void TC_1_() {
+		
+	}
+	
+	@AfterClass
+	public void AfterClass() {
+		
+	}
+}
