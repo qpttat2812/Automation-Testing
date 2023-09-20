93b895506dc918d5176115357fc6fe4f3520b474
ttqp2812fi <qptt2812@gmail.com>
2023-09-20 14:59:04 +0700

    rename file

--- src/webdriver/Topic_23_DemoWait.java
+++ src/webdriver/Topic_23_DemoWait.java
@@ -14,7 +14,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic24_DemoWait {
+public class Topic_23_DemoWait {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
