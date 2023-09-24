902102a74072f6d561164d15f219cc950faf0fab
ttqp2812fi <qptt2812@gmail.com>
2023-09-24 15:17:47 +0700

    set up test-ng

---
+++ src/testng/Topic_01_Annotation.java
@@ -0,0 +1,26 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Topic_01_Annotation {
+	
+	@org.testng.annotations.BeforeClass
+	public void BeforeClass() {
+		
+	}
+	
+	@Test
+	public void TC_01() {
+		
+	}
+	
+	@Test
+	public void TC_02() {
+		
+	}
+	
+	@org.testng.annotations.AfterClass
+	public void AfterClass() {
+		
+	}
+}
